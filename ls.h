811cf47e644c24789379ea2ecb5c648eccb14314
AbhaySachan0 <abhaysach05@gmail.com>
2026-02-03 14:51:42 +0530

    added sorting by file_name

--- ls.h
+++ ls.h
@@ -1,6 +1,9 @@
 
 #include<dirent.h>
 
+#include<string.h>
+#define MAX_ENTRY 100
+
 typedef struct {
     int show_all; // -a
     int show_inode; // -i
@@ -9,10 +12,42 @@ typedef struct {
     char *path; // stores path
 } options;
 
-void show_inode(const unsigned long inode[],int n) {
+typedef struct {
+    unsigned long inode[MAX_ENTRY];
+    char *file_name[MAX_ENTRY];
+    char *permission[MAX_ENTRY];
+    char *user[MAX_ENTRY];
+    int entry_count;
+}Data;
+
+
+void sort_by_filename(Data *data) {
+    for(int i=0;i<data->entry_count-1;i++){
+        for(int j=i+1;j<data->entry_count;j++) {
+            if(strcmp(data->file_name[i],data->file_name[j]) > 0) { // swap every entry
+
+                // inode
+                unsigned long temp_inode = data->inode[i];
+                data->inode[i] = data->inode[j];
+                data->inode[j] = temp_inode;
+                
+                //file name
+                char *temp = data->file_name[i];
+                data->file_name[i] = data->file_name[j];
+                data->file_name[j] = temp;
+            }   
+        }
+    }
+}
+
+
+void show_inode(const Data data, options *opt) {
     
-    for(int i=0;i<n;i++){
-        printf("%lu\n",(unsigned long)inode[i]);
+    for(int i=0;i<data.entry_count;i++){
+        char *name = data.file_name[i];
+        
+        if(!(opt->show_all) && name[0]=='.') continue;
+        printf("%lu  %s\n",data.inode[i],data.file_name[i]);
     }
 
 }
