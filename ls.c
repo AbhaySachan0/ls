89cbd9f8cfaa937d984dd865f28716c1e53284b3
AbhaySachan0 <abhaysach05@gmail.com>
2026-02-05 15:00:36 +0530

    improved code (used functions)

--- ls.c
+++ ls.c
@@ -1,96 +1,38 @@
 #include<stdio.h>
-#include<dirent.h>
 #include"ls.h"
 #include<string.h>
 #include<stdlib.h>
+#include<time.h>
 
 // for user and group name
-#include<pwd.h>
-#include<grp.h>
 
-#include <time.h>
-
-
-#include<unistd.h>
 #include<sys/stat.h> // for stat()
 
 int main(int argc, char *argv[]) {
     options opt = {0};
 
-    for(int i=1;i<argc;i++) {
-        if(argv[i][0] == '-') { // flag
-            // multiple flags together
-            for(int j=1;argv[i][j]!='\0';j++) {
-                switch (argv[i][j]) {
-                    case 'a':
-                        opt.show_all = 1;
-                        break;
-                    case 'l':
-                        opt.show_long_list= 1;
-                        break;
-                    case 'i':
-                        opt.show_inode= 1;
-                        break;
-                }
-                
-            }
-        } else { // path
-            opt.path_count++;
-            opt.path = argv[i];
-        }  
-    }
-
-    if(opt.path_count==0) {
-        opt.path = ".";
-        
-    }
-
-    printf("Hello\n");
-
-    // printf("-a: %d\n-l: %d\n -i: %d\n",opt.show_all,opt.show_long_list,opt.show_inode);
-    // printf("Path : %s\n",opt.path);
-
-    // opening and reading the directory 
+    command_parse(argc,argv,&opt);
 
     Data data = {0};
+    scan_directory(&data, opt);
+
+    printf("total : %d\n",data.entry_count);
+    // for(int i=0;i<data.entry_count;i++) {
+    //     printf("%s  %lu  %s  %s  %s  %s\n",data.permission[i],data.inode[i],data.user[i],data.group[i],data.mod_time[i],data.file_name[i]);
+    // }
+
+
+    // struct stat st;
+
+    // printf("%s HEllo world",ctime(&st.st_mtime));
+
     
-    DIR *dir = opendir(opt.path);
-    struct dirent *entry;
-
-    int i=0;
-    while((entry = readdir(dir)) !=NULL) {
-        struct stat st;
-        // stat(entry->d_name,&st);
-        if (stat(entry->d_name, &st) == -1) {
-            perror("stat");
-            continue;
-        }
-
-        struct passwd *pw = getpwuid(st.st_uid);
-        struct group  *gr = getgrgid(st.st_gid);
-        
-        data.file_name[i] = strdup(entry->d_name);
-
-        data.group[i] = strdup(gr->gr_name); 
-        data.user[i] = strdup(pw->pw_name); 
-        data.userID[i] = st.st_uid;
-        data.mod_time[i] = strdup(ctime(&st.st_mtime));
-        
-        // working on permission
-        char perm[11];
-        permission(st.st_mode,perm);
-        data.permission[i] = strdup(perm);
-
-        // printing
-        // printf("%s\n",data.permission[i]);
-        // printf("Modified: %s", ctime(&st.st_mtime));
-        // printf("%d\n",data.userID[i]);
-        // printf("%s\n",data.group[i]);
-        
-        i++;
-        data.entry_count++;
-    }
-
-    // sort_by_filename(&data);
+    sort_by_filename(&data);
+    show_inode(data,opt);
+    // int show_all; // -a
+    // int show_inode; // -i
+    // int show_long_list; // -l
+    // int path_count; // check if there is path or not
+    // char *path; // stores path
 }
 
